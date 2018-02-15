94d6e652cc85ec32e4a1cc9c585b202816c510a5
MohannadNaj <me@mohannad.io>
2018-02-15 06:20:20 +0300

    disable vue.config.productionTip

--- src/karma-eslint-stubs/specs/ExampleComponent.spec.js
+++ src/karma-eslint-stubs/specs/ExampleComponent.spec.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import ExampleComponent from '../resources/assets/js/components/ExampleComponent';
 
+// alternatively, you can instantiate Vue from the 'setup/index.js' file
+Vue.config.productionTip = false;
+
 describe('ExampleComponent.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(ExampleComponent);
